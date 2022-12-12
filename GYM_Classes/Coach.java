1c08db40634aeb79c076b74465e714c9198b493e
pl2-Ahmed <120251316+pl2-Ahmed@users.noreply.github.com>
2022-12-12 23:02:27 +0200

    Update Coach.java

--- GYM_Classes/Coach.java
+++ GYM_Classes/Coach.java
@@ -4,28 +4,28 @@ import java.util.Date;
 
 public class Coach extends User {
     /*start data fields*/
-    private double sallary;
+    private double salary;
     /*end data fields*/
     //***********************************************************************
     /* start constractoers */
-    public Coach(int id, String filrstName, String lastName, String email, String password, String address, String phoneNumber, Date birthDate, double sallary)
+    public Coach(int id, String filrstName, String lastName, String email, String password, String address, String phoneNumber, Date birthDate, double salary)
     {
         super( id , filrstName , lastName , email , password , address , phoneNumber , birthDate );
-        this.sallary = sallary;
+        this.salary = salary;
     }
     /* end constractoers */
     //***********************************************************************
     /*start seter*/
 
-    public void setSallary(double sallary) {
-        this.sallary = sallary;
+    public void setSallary(double salary) {
+        this.salary = salary;
     }
     /*start seter*/
     //***********************************************************************
     /*start geter*/
 
-    public double getSallary() {
-        return sallary;
+    public double getSalary() {
+        return salary;
     }
     /*end geter*/
     //***********************************************************************
