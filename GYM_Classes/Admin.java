2124c224af33795defe13f20e8da15ff1039763c
pl2-Ahmed <120251316+pl2-Ahmed@users.noreply.github.com>
2022-12-12 23:02:04 +0200

    Update Admin.java

--- GYM_Classes/Admin.java
+++ GYM_Classes/Admin.java
@@ -4,28 +4,28 @@ import java.util.Date;
 
 public class Admin extends User {
     /*start data fields*/
-    double sallary;
+    double salary;
     /*end data fields*/
     //***********************************************************************
     /* start constractoers */
-    public Admin(int id, String filrstName, String lastName, String email, String password, String address, String phoneNumber, Date birthDate, double sallary)
+    public Admin(int id, String filrstName, String lastName, String email, String password, String address, String phoneNumber, Date birthDate, double salary)
     {
         super( id , filrstName , lastName , email , password , address , phoneNumber , birthDate );
-        this.sallary = sallary;
+        this.salary = salary;
     }
     /* end constractoers */
     //***********************************************************************
     /*start seter*/
 
-    public void setSallary(double sallary) {
-        this.sallary = sallary;
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
     /*start seter*/
     //***********************************************************************
     /*start geter*/
 
-    public double getSallary() {
-        return sallary;
+    public double getSalary() {
+        return salary;
     }
     /*end geter*/
     //***********************************************************************
