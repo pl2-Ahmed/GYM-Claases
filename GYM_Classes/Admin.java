eaa2344ad22220ff704adf41b6819ec6a22007b2
pl2-Ahmed <120251316+pl2-Ahmed@users.noreply.github.com>
2022-12-12 11:11:38 +0200

    Update Admin.java

--- GYM_Classes/Admin.java
+++ GYM_Classes/Admin.java
@@ -1,23 +1,32 @@
 package GYM_Classes;
 
+import java.util.Date;
+
 public class Admin extends User {
     /*start data fields*/
-
+    double sallary;
     /*end data fields*/
     //***********************************************************************
     /* start constractoers */
-    public Admin(int id,String name,String password)
+    public Admin(int id, String filrstName, String lastName, String email, String password, String address, String phoneNumber, Date birthDate, double sallary)
     {
-        super(id,name,password);
+        super( id , filrstName , lastName , email , password , address , phoneNumber , birthDate );
+        this.sallary = sallary;
     }
     /* end constractoers */
     //***********************************************************************
     /*start seter*/
 
+    public void setSallary(double sallary) {
+        this.sallary = sallary;
+    }
     /*start seter*/
     //***********************************************************************
     /*start geter*/
 
+    public double getSallary() {
+        return sallary;
+    }
     /*end geter*/
     //***********************************************************************
 }
